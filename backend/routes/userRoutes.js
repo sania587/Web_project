59652f2d67e1f8bf3717c238692505de83f3aa83
sania587 <124341621+sania587@users.noreply.github.com>
2024-12-08 11:04:55 +0500

    merge schema files with main

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,15 +8,5 @@ router.post('/signup', signup);
 
 // Login route
 router.post('/login', login);
-router.route('/send-mail').post(sendMail);
-router.route('/authenticate').post(middleware.verifyUser, (req, res) => res.end());
-
-// GET Methods
-router.route('/generate-otp').get(middleware.verifyUser, middleware.localVariables, controller.generateOTP);
-router.route('/verify-otp').get(middleware.verifyUser, controller.verifyOTP);
-router.route('/create-reset-session').get(controller.createResetSession);
-
-// PUT Methods
-router.route('/reset-password').put(middleware.verifyUser, controller.resetPassword);
 
 module.exports = router;
